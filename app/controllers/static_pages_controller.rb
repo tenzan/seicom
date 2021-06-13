08257b5bd3826c38f50a527f0f1854c6cb225627
Askar Karasaev <askar.karasaev@gmail.com>
2021-06-13 13:20:44 +0900

    Add a Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def selling
+  end
+
+  def buying
+  end
+
+  def lending
+  end
+
+  def renting
+  end
+
+  def managing
+  end
+
+  def property0
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/selling'
+  get 'static_pages/buying'
+  get 'static_pages/lending'
+  get 'static_pages/renting'
+  get 'static_pages/managing'
+  get 'static_pages/property0'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
